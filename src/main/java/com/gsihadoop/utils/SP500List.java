cd588f9a7ea05652e46d9bfc7523a3d2d542785d
jmd81689 <jmd81689@yahoo.com>
2014-03-06 08:40:23 -0500

    Added quick and sloppy HashSet implementation for search performance gains.

--- src/main/java/com/gsihadoop/utils/SP500List.java
+++ src/main/java/com/gsihadoop/utils/SP500List.java
@@ -3,12 +3,14 @@ package com.gsihadoop.utils;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 
 public class SP500List {
 	private static String[] list;
-	
+	private static HashSet<String> hs;
 	private SP500List(){
 		list = new String[500];
 	}
@@ -31,9 +33,16 @@ public class SP500List {
 			tempList.add(line.trim());
 		}
 		list = tempList.toArray(new String[tempList.size()]);
+		Arrays.sort(list);
 	}
 
 	public String[] getSP500List(){
 		return list;
+	}
+	
+	public static HashSet<String> getSP500HashSet(){
+		HashSet<String> mySet = new HashSet<>(Arrays.asList(list));
+		hs = mySet;
+		return hs;
 	}
 }
