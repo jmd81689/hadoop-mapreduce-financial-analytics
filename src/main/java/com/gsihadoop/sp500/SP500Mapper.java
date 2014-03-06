bc9c21460bb5f2187cdd481acc8f3b8157692b98
jmd81689 <jmd81689@yahoo.com>
2014-03-06 08:54:49 -0500

    Implemented multiple key/record mapper for yearly and monthly aggregates.  Passed MRUnit testing

--- src/main/java/com/gsihadoop/sp500/SP500Mapper.java
+++ src/main/java/com/gsihadoop/sp500/SP500Mapper.java
@@ -5,8 +5,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.Arrays;
+import java.util.HashSet;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -14,24 +16,34 @@ import org.apache.hadoop.mapreduce.Mapper;
 import com.gsihadoop.utils.SP500List;
 import com.gsihadoop.utils.StockData;
 
-public class SP500Mapper extends Mapper<LongWritable, Text, Text, Text> {
+public class SP500Mapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
 	
 	@Override
 	public void map(LongWritable key, Text value, Context context){
 		Configuration conf = context.getConfiguration();
 		
 		Path path = Paths.get(conf.get("sp500Path"));
-		
 		String[] sp500List = SP500List.parseFile(path);
+		HashSet<String> sp500HashSet = SP500List.getSP500HashSet();
 		String line = value.toString();
-		
 		try {
+			// May need to add firstline detection here
 			StockData record = StockData.parse(line);
 			String symbol = record.getStock_symbol();
-			if (Arrays.binarySearch(sp500List, symbol) >= 0){
-				String outputKey = symbol + "," + record.getDate().substring(0,4);
+			System.out.println(line);
+			if (sp500HashSet.contains(symbol)){
+				/*String outputKey = symbol + "," + record.getDate().substring(0,4);
 				String outputValue = record.getDate()+ "," + record.getStock_price_close();
-				context.write(new Text(outputKey), new Text(outputValue));
+				context.write(new Text(outputKey), new Text(outputValue));*/
+				
+				String yearlyAggKey = symbol + "," + record.getDate().substring(0,4);
+				//String yearlyAggValue = "" + record.getStock_price_close();
+				//context.write(new Text(yearlyAggKey), new Text(yearlyAggValue));
+				context.write(new Text(yearlyAggKey), new DoubleWritable(record.getStock_price_close()));
+				String monthlyAggKey = symbol + "," + record.getDate().substring(0,7);
+				//String monthlyAggValue = "" + record.getStock_price_close();
+				//context.write(new Text(monthlyAggKey), new Text(monthlyAggValue));
+				context.write(new Text(monthlyAggKey), new DoubleWritable(record.getStock_price_close()));
 			}
 		} catch(ArrayIndexOutOfBoundsException e){
 			e.printStackTrace();
